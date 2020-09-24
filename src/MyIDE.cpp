65e5b4bf3175a3d46ae38127b26334112c77e690
CPaulline <colep41@gmail.com>
2020-09-24 17:32:50 -0500

    Added the class being used for the program, and started the file reader which reads each line into atemp string to be further analyzed

--- src/MyIDE.cpp
+++ src/MyIDE.cpp
@@ -9,6 +9,33 @@
 #include <iostream>
 using namespace std;
 
+class LexAnalyzer{
+private:
+	vector <string> lexemes;
+	vector <string> tokens;
+	map <string, string> tokenmap;
+
+public:
+	LexAnalyzer(istream& infile);
+
+	void scanFile(istream& infile, ostream& outfile){
+		if(!infile){
+			cout << "could not open the file" << endl;
+			exit(-1);
+		}
+		int i = 0;
+		string s;
+		while(getline(infile, s)){
+
+			//scan through the line stored in 's' to split keywords from symbols and integers
+
+			i++;
+		}
+
+		cout << "file read successfully" << endl;
+	}
+};
+
 int main() {
 	cout << "Hello World" << endl; // prints Hello World
 	return 0;
